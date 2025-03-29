b577be843d1bd0f07ad59b0e533b10f9ea1e71c2
Antonio Queb <antonioqueb@alphaqueb.online>
2025-03-29 10:13:27 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -4,6 +4,7 @@
     "category": "Custom",
     "depends": ["sale", "residuos_autorizados_partner"],
     "data": [
+        "security/ir.model.access.csv",
         "data/sequence.xml",
         "views/manifiesto_views.xml",
     ],

d8e768ca2cb6434a0efc72973ebfcaeefe07523f
Antonio Queb <antonioqueb@alphaqueb.online>
2025-03-29 18:41:06 -0600

    Up

--- __manifest__.py
+++ __manifest__.py
@@ -7,7 +7,6 @@
     "security/ir.model.access.csv",
     "data/sequence.xml",
     "report/manifiesto_pdf.xml",  # Primero plantilla
-    "report/report_actions.xml",  # Luego acción del reporte
     "views/manifiesto_views.xml", # Finalmente las vistas
     ],
 
